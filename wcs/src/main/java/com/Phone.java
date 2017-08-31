503ee10e6b4765db2929404dba119eea6f2c6f1d
Anjali <anjratho@in.ibm.com>
2017-08-31 16:18:32 +0530

    Addition to test

---
+++ wcs/src/main/java/com/Phone.java
@@ -0,0 +1,25 @@
+package com;
+
+import com.ibm.keymanager.audit.p;
+
+class Phone {
+	String model;
+	String company;
+	Phone(String model) {
+		this.model=model;
+		System.out.println(model);
+	}
+	double wt;
+ void makeCall(String num){
+		//code
+	 return;
+	}
+	void recvCall(){
+
+	}
+	public static void main(){
+		
+	}
+	
+
+}
