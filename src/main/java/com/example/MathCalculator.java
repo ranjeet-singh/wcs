fa315891eed4e261fd7517e1e3a81ce95fd653f9
Ranjeet <singh.ranjeet61@gmail.com>
2017-09-01 10:34:26 +0530

    Initial project setup

---
+++ src/main/java/com/example/MathCalculator.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import com.example.exception.InvalidValueException;
+
+public class MathCalculator {
+	
+	public int sum(int a,int b){
+		System.out.println("Inside MathCalculator.sum(a,b)");
+		return a+b;
+	}
+
+	public int diff(int a,int b){
+		return a-b;
+	}
+
+	public int divide(int a,int b) throws InvalidValueException{
+		if(a==0 || b==0){
+			throw new InvalidValueException("Value can not be zero!!!!");
+		}
+		return a/b;
+	}
+
+}
